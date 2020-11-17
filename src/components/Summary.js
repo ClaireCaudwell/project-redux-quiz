7b1bbc6fcce189713662c727ed462350a03b94b1
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-11-17 14:47:40 +0100

    Restart button implemented on Summary component

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { quiz } from '../reducers/quiz'
 
 export const Summary = () => {
   const correctAnswers = useSelector((state) => state.quiz.answers.filter((answer) => answer.isCorrect === true).length)
+
+  const dispatch = useDispatch()
+
+  const restart = () => {
+    dispatch(quiz.actions.restart())
+  }
 
   return (
     <>
       <h1>Summary Page - replace with a Summary component (include restart button in summary</h1>
       <p>{`You've got ${correctAnswers} out of 5.`}</p>
+      <button type="button" onClick={restart}>RESTART</button>
     </>
   )
 }
